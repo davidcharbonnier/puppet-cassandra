80ff392a2398dcd7f7c0ba9bc888bc038d7e2b2d
David Charbonnier <david.charbonnier@rg-systemes.com>
2014-10-24 10:58:18 +0200

    Removing key parameter from debian repository definition

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -10,7 +10,6 @@ class cassandra::repo::debian(
         location          => $location,
         release           => $release,
         repos             => $repos,
-        key               => $repo_name,
         key_source        => $key_source,
         pin               => $pin,
         include_src       => false,
